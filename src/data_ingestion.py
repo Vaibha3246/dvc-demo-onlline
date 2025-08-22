f7d228606c44a02a68df2e96b1d51281be7f3ee9
vaibhav <vaibhavkumar8979581578@gmail.com>
2025-08-22 16:31:43 +0530

    initial commit project setup by dvc and git

--- src/data_ingestion.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import os  
-df=pd.read_csv('C:/Users/vaibh/Downloads/Customer Segmentation.csv')
-df.drop(columns=['ID'],inplace=True)
-df=df[df['Age']>30]
-df=df[df['Work_Experience']>1]
-df.to_csv(os.path.join('data','customer.csv')) 
