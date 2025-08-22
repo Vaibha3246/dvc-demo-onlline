0bc7441c11b1fb325816f8a8ed239f05a4c9bce8
vaibhav <vaibhavkumar8979581578@gmail.com>
2025-08-22 17:16:17 +0530

    add 2nd commit

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -3,6 +3,6 @@ import numpy as np
 import os
 df=pd.read_csv('C:/Users/vaibh/Downloads/Customer Segmentation.csv')
 df.drop(columns=['ID'],inplace=True)
-df = df[(df['Age'] > 30) & (df['Work_Experience'] > 1)]
+df = df[(df['Age'] > 25) & (df['Work_Experience'] > 0)]
 
 df.to_csv(os.path.join('data','employee.csv'),index=False)
